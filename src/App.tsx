a0c9afd1f6803153f7eedb698bb0b3372f021c18
Renan Torres <rtorres@daitan.com>
2022-07-26 09:37:02 -0300

    [TSTORE]: First commit, bootstrapped project with CRA+TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Start of TypeScript Store example.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
