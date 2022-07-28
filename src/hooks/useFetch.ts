586b956fb95c3ab10d12f61864d5cfa4bae863b7
Renan Torres <rtorres@daitan.com>
2022-07-28 09:09:49 -0300

    [TSTORE]: Implemented data fetching with XMLHttpRequest function in useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,15 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 const useFetch = (endpoint: string) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(endpoint)
-      .then((res) => res.json())
-      .then((json) => setData(json));
+    getDataWithXmlHttpReq(endpoint, setData);
   }, [endpoint]);
 
   return data;
+};
+
+const getDataWithXmlHttpReq = (
+  endpoint: string,
+  setData: Dispatch<SetStateAction<never[]>>,
+) => {
+  const xmlReq = new XMLHttpRequest();
+  xmlReq.onreadystatechange = () => {
+    if (xmlReq.readyState === 4 && xmlReq.status === 200) {
+      setData(Array.from(JSON.parse(xmlReq.response)));
+    }
+  };
+  xmlReq.open('GET', endpoint);
+  xmlReq.send();
+};
+
+const getDataWithFetchAPI = (
+  endpoint: string,
+  setData: Dispatch<SetStateAction<never[]>>,
+) => {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((json) => setData(json));
 };
 
 export default useFetch;
